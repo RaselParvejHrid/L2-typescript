a271143076f371ada4ca81a977ad02f8dbe25df6
Rasel Parvej <RaselParvejHrid@gmail.com>
2025-05-08 12:54:41 +0600

    Task 2 done

--- solution.ts
+++ solution.ts
@@ -5,6 +5,8 @@ function formatString(input: string, toUpper?: boolean): string {
   return input.toUpperCase();
 }
 
-console.log(formatString("Hello"));
-console.log(formatString("Hello", true));
-console.log(formatString("Hello", false));
+function filterByRating(
+  items: { title: string; rating: number }[]
+): { title: string; rating: number }[] {
+  return items.filter((item) => item.rating >= 4);
+}
