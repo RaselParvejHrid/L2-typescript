6cacdc00713988d0d2d73cc1910c95f47e53f0f7
Rasel Parvej <RaselParvejHrid@gmail.com>
2025-05-08 12:48:21 +0600

    Task 1 done

---
+++ solution.ts
@@ -0,0 +1,10 @@
+function formatString(input: string, toUpper?: boolean): string {
+  if (toUpper === false) {
+    return input.toLowerCase();
+  }
+  return input.toUpperCase();
+}
+
+console.log(formatString("Hello"));
+console.log(formatString("Hello", true));
+console.log(formatString("Hello", false));
